6620998db0e9e6fec5742019ab481a7dcd060c2b
Medkinson Medvezhkin <148875917+medkinson@users.noreply.github.com>
2025-05-31 18:20:11 +0300

    created "item" variable

--- TpToItem.lua
+++ TpToItem.lua
@@ -1,12 +1,13 @@
 local player = game:GetService("Players").LocalPlayer
 local character = player.Character
+local item = "(P320)"
 
 for _, v in workspace:GetChildren() do
     if v.Name == "Worldmodel" then
         local pp = v:FindFirstChildWhichIsA("ProximityPrompt")
         local handle = v:FindFirstChild("Handle")
         
-        if pp and pp.ObjectText == "(P320)" and handle and handle:IsA("MeshPart") then
+        if pp and pp.ObjectText == item and handle and handle:IsA("MeshPart") then
             local root = character:FindFirstChild("HumanoidRootPart")
             if root then
                 local savedCFrame = root.CFrame
